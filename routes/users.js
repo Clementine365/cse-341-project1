a615ea54ab736a88c0241f5faba1d9225f6b28aa
Clementine.Iradukunda <clementineiradu365@gmail.com>
2025-03-10 12:15:08 +0200

    all

--- routes/users.js
+++ routes/users.js
@@ -1,2 +1,11 @@
 const express = require('express');
 const router = express.Router();
+
+const userscontroller = require('../controllers/users');
+
+router.get('/', userscontroller.getAll);
+
+router.get('/:id', userscontroller.getSingle);
+
+module.exports = router;
+
